196cb299eea00827668fde356976ac0c9ad1f126
anjali <anjaliachalla@gmail.com>
2018-09-25 20:41:58 -0500

    Connected to git changed hello world

---
+++ src/main/java/io/egen/springifypro/App.java
@@ -0,0 +1,13 @@
+package io.egen.springifypro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello print World!" );
+    }
+}
---
+++ src/main/java/io/egen/springifypro/Mainclass.java
@@ -0,0 +1,10 @@
+package io.egen.springifypro;
+
+public class Mainclass {
+	
+	public static void main(String args[])
+	{
+		System.out.println("HAppy girl");
+	}
+
+}
