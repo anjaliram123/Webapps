f0ae11d7bc1de515d7ee4d30dffbfc6b40e2935f
anjali <anjaliachalla@gmail.com>
2018-11-09 17:58:40 -0600

    Added a new class Mainappclass

---
+++ src/test/java/io/egen/springifypro/Mainappclass.java
@@ -0,0 +1,11 @@
+package io.egen.springifypro;
+
+public class Mainappclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Some other user added main app class");
+	}
+
+}
