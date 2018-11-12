e23f1625f41261c57ff9d933e9190a862bdfa51b
anjali <anjaliachalla@gmail.com>
2018-11-11 18:34:42 -0600

    changed a line in app class

--- src/main/java/io/egen/springifypro/App.java
+++ src/main/java/io/egen/springifypro/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello print World!" );
+        System.out.println("Changed a line in App.java to see if it gets reflected");
     }
 }
